823222d7b8fc6c8f777739c8b8791b10c800f6aa
rhildred <rhildred@gmail.com>
2024-04-11 18:41:15 -0400

    half pasat wine

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/salt", async(req, res)=>{
+    res.send(`request ${JSON.stringify(req.body)}`);
+});
 const server = app.listen(8080, ()=>{
     console.log(`listening on http://localhost:${server.address().port}`);
 })
