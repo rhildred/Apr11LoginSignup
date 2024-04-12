8d3d68edd0f8ae8a86bc22e5d21f7ae0bc78f464
Rich Hildred <rhildred@wlu.ca>
2024-04-12 00:45:18 -0400

    calculate md5

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    .replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, 
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
 app.post("/salt", async(req, res)=>{
-    res.send(`request ${JSON.stringify(req.body)}`);
+    res.send({salt:uuidv4()});
 });
 const server = app.listen(8080, ()=>{
     console.log(`listening on http://localhost:${server.address().port}`);
